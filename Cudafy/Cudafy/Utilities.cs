eff19f818a8d5b668fa7212a6bfed1be16b093c2
SND\NickKopp_cp <SND\NickKopp_cp@2b2adeab-ffde-484a-ad65-8eada40eade2>
2011-12-12 09:19:55 +0000

    Release V1.6
    git-svn-id: https://cudafy.svn.codeplex.com/svn@7431 2b2adeab-ffde-484a-ad65-8eada40eade2

--- Cudafy/Cudafy/Utilities.cs
+++ Cudafy/Cudafy/Utilities.cs
@@ -34,9 +34,9 @@ namespace Cudafy
     public static class CV
     {
         /// <summary>
-        /// Version 1.5.*
+        /// Version 1.6.*
         /// </summary>
-        public const string csVERSION = "1.5.*";
+        public const string csVERSION = "1.6.*";
     }
 
     /// <summary>
